47e6574bff5800a974822092a38a0634e8c2b5b7
Brian Plummer <brian.r.plummer@gmail.com>
2014-12-20 22:12:22 -0500

    impl of aes 128 bit key that is salted

--- encryption/src/main/java/se/simbio/encryption/Encryption.java
+++ encryption/src/main/java/se/simbio/encryption/Encryption.java
@@ -17,10 +17,17 @@ import android.R.string;
 import android.util.Base64;
 import android.util.Log;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 
 /**
  * A class to make more easy simple encrypt routines
@@ -30,8 +37,18 @@ public class Encryption {
     private static final String TAG = "Encryption";
 
     private String mCharsetName = "UTF8";
-    private String mAlgorithm = "DES";
     private int mBase64Mode = Base64.DEFAULT;
+
+    //type of aes key that will be created
+    private String SECRET_KEY_TYPE = "PBKDF2WithHmacSHA1";
+    //value used for salting....can be anything
+    private String salt = "some_salt";
+    //length of key
+    private int KEY_LENGTH = 128;
+    //number of times the password is hashed
+    private int ITERATION_COUNT = 65536;
+    //main family of aes
+    private String mAlgorithm = "AES";
 
     /**
      * @return the charset name
@@ -86,9 +103,7 @@ public class Encryption {
     public String encrypt(String key, String data) {
         if (key == null || data == null) return null;
         try {
-            DESKeySpec desKeySpec = new DESKeySpec(key.getBytes(mCharsetName));
-            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance(mAlgorithm);
-            SecretKey secretKey = secretKeyFactory.generateSecret(desKeySpec);
+            SecretKey secretKey = getSecretKey(hashTheKey(key));
             byte[] dataBytes = data.getBytes(mCharsetName);
             Cipher cipher = Cipher.getInstance(mAlgorithm);
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
@@ -111,9 +126,7 @@ public class Encryption {
         if (key == null || data == null) return null;
         try {
             byte[] dataBytes = Base64.decode(data, mBase64Mode);
-            DESKeySpec desKeySpec = new DESKeySpec(key.getBytes(mCharsetName));
-            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance(mAlgorithm);
-            SecretKey secretKey = secretKeyFactory.generateSecret(desKeySpec);
+            SecretKey secretKey = getSecretKey(hashTheKey(key));
             Cipher cipher = Cipher.getInstance(mAlgorithm);
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
             byte[] dataBytesDecrypted = (cipher.doFinal(dataBytes));
@@ -124,4 +137,45 @@ public class Encryption {
         }
     }
 
+    /**
+     * creates a 128bit salted aes key
+     *
+     * @param key encoded input key
+     * @return aes 128 bit salted key
+     * @throws NoSuchAlgorithmException
+     * @throws UnsupportedEncodingException
+     * @throws InvalidKeySpecException
+     */
+    private SecretKey getSecretKey(char[] key) throws NoSuchAlgorithmException,
+            UnsupportedEncodingException,
+            InvalidKeySpecException {
+        SecretKeyFactory factory = null;
+        factory = SecretKeyFactory.getInstance(SECRET_KEY_TYPE);
+
+        KeySpec spec = new PBEKeySpec(key,
+                salt.getBytes(mCharsetName),
+                ITERATION_COUNT,
+                KEY_LENGTH);
+
+        SecretKey tmp = factory.generateSecret(spec);
+        return new SecretKeySpec(tmp.getEncoded(), mAlgorithm);
+    }
+
+    /**
+     * takes in a simple string and performs an sha1 hash
+     * that is 128 bits long...we then base64 encode it
+     * and return the char array
+     *
+     * @param key simple inputted string
+     * @return sha1 base64 encoded representation
+     * @throws UnsupportedEncodingException
+     * @throws NoSuchAlgorithmException
+     */
+    private char[] hashTheKey(String key) throws UnsupportedEncodingException,
+            NoSuchAlgorithmException {
+
+        MessageDigest md = MessageDigest.getInstance("SHA1");
+        md.update(key.getBytes(mCharsetName));
+        return Base64.encodeToString(md.digest(),Base64.NO_PADDING).toCharArray();
+    }
 }
